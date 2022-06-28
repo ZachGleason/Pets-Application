c71840db23689dbff91bc0daa8602962a50efa88
XiaoGao99 <advise2013@hotmail.com>
2022-06-27 17:31:30 -0700

    Finish type routes

--- server/controller/type.controller.js
+++ server/controller/type.controller.js
@@ -1,18 +1,71 @@
 const { connection } = require("../config/mysql.config");
 
 module.exports = {
-  getAllType : (req, res) => {
-    connection.query("SELECT * FROM Type;", (err, result) => {
-      if (err) {
-        res.status(200).json({
-          status : 'error',
-          message : err
-        })
-      } else {
-        res.status(200).json({
-          data : result
-        })
-      }
-    })
-  }
-}
+	getAllType: (req, res) => {
+		connection.query("SELECT * FROM Type;", (err, result) => {
+			if (err) {
+				res.status(200).json({
+					status: "error",
+					message: err,
+				});
+        throw err;
+			} else {
+				res.status(200).json({
+					data: result,
+				});
+			}
+		});
+	},
+
+	addNewType: (req, res) => {
+		let query = "INSERT INTO Type (name) VALUES (?);";
+		connection.query(query, [req.body.name], (err, result) => {
+			if (err) {
+				res.status(201).json({
+					status: "error",
+					message: err,
+				});
+        throw err;
+			} else {
+				res.status(201).json({
+					data: result,
+				});
+			}
+		});
+	},
+
+	editTypeById: (req, res) => {
+		let query = "UPDATE Type SET name = ? WHERE id = ?;";
+		connection.query(query, [req.body.name, req.params.id], (err, result) => {
+			if (err) {
+				res.status(201).json({
+					status: "error",
+					message: err,
+				});
+
+        throw err;
+			} else {
+				res.status(201).json({
+					data: result,
+				});
+			}
+		});
+	},
+
+	deleteTypeById: (req, res) => {
+		let query = "DELETE FROM Type WEHRE id = ?;";
+		connection.query(query, [req.params.id], (err, result) => {
+			if (err) {
+				res.status(200).json({
+					status: "error",
+					message: err,
+				});
+        throw err;
+			} else {
+				res.status(200).json({
+					data: result,
+				});
+			}
+		});
+	},
+};
--- server/routes/type.routes.js
+++ server/routes/type.routes.js
@@ -1,5 +1,8 @@
-const TypeController = require("../controller/type.controller")
+const TypeController = require("../controller/type.controller");
 
 module.exports = (app) => {
-  app.get("/api/type", TypeController.getAllType)
-}
+	app.get("/api/type", TypeController.getAllType);
+	app.post("/api/type", TypeController.addNewType);
+	app.put("/api/type/:id", TypeController.editTypeById);
+	app.delete("/api/type/:id", TypeController.deleteTypeById);
+};
