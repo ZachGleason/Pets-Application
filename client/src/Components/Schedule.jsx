dd5e541695025544203d5f4042534273c8d7f998
Ana Gomez <anagomez@Anas-MacBook-Pro.local>
2022-06-27 23:42:31 -0500

    textfields and checkboxes

--- client/src/Components/Schedule.jsx
+++ client/src/Components/Schedule.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Select from 'react-select';
 import { FaEdit } from "react-icons/fa";
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import { FormControl, FormControlLabel } from '@mui/material';
 
 const Schedule = () => {
 
@@ -27,20 +30,72 @@ const Schedule = () => {
     </div>
     <div style={{height: 480, overflow: "scroll"}}>
         {/* Map out all of the scheduled boxes for the selected pet */}
-        {/* Meantime these are hardcoded boxes to display what should be expected */}
-        <div style={{ width: "95%", height: 110, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <div style={{ width: "95%", height: 160, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <TextField id="standard-basic" label="Task:" variant="standard" sx={{mb:1, ml:5, mt:-1}} />
+        <br/>
+        <TextField id="standard-basic" label="Time:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <br/>
+        <TextField id="standard-basic" label="Frencuency:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <FormControlLabel sx={{mt:2, ml:30, mt:-1, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Completed" />
+        <br/>
+        <FormControlLabel sx={{mt:-28, ml: 56, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Daily" />
 
         </div>
-        <div style={{ width: "95%", height: 110, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <div style={{ width: "95%", height: 160, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <TextField id="standard-basic" label="Task:" variant="standard" sx={{mb:1, ml:5, mt:-1}} />
+        <br/>
+        <TextField id="standard-basic" label="Time:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <br/>
+        <TextField id="standard-basic" label="Frencuency:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <FormControlLabel sx={{mt:2, ml:30, mt:-1, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Completed" />
+        <br/>
+        <FormControlLabel sx={{mt:-28, ml: 56, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Daily" />
 
         </div>
-        <div style={{ width: "95%", height: 110, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <div style={{ width: "95%", height: 160, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <TextField id="standard-basic" label="Task:" variant="standard" sx={{mb:1, ml:5, mt:-1}} />
+        <br/>
+        <TextField id="standard-basic" label="Time:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <br/>
+        <TextField id="standard-basic" label="Frencuency:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <FormControlLabel sx={{mt:2, ml:30, mt:-1, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Completed" />
+        <br/>
+        <FormControlLabel sx={{mt:-28, ml: 56, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Daily" />
 
         </div>
-        <div style={{ width: "95%", height: 110, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
-
-        </div>
-        <div style={{ width: "95%", height: 110, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <div style={{ width: "95%", height: 160, border: "1px solid black", borderRadius: 15, marginLeft: 10, marginTop: 8 }}>
+        <TextField id="standard-basic" label="Task:" variant="standard" sx={{mb:1, ml:5, mt:-1}} />
+        <br/>
+        <TextField id="standard-basic" label="Time:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <br/>
+        <TextField id="standard-basic" label="Frencuency:" variant="standard" sx={{mb:1, ml:5, mt:-1}}/>
+        <FormControlLabel sx={{mt:2, ml:30, mt:-1, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Completed" />
+        <br/>
+        <FormControlLabel sx={{mt:-28, ml: 56, width: "30%"}} control={
+        <Checkbox defaultChecked />
+        }
+        label="Daily" />
 
         </div>
     </div>
